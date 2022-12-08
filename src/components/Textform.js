6c0ea139a9cd7224b32ca56249320eafb26e0431
Divas Kumar Gupta <dvsmathur7050@gmail.com>
2022-12-09 01:45:19 +0530

    added real time count of words and character

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -14,16 +14,25 @@ export default function TextForm(props) {
         // console.log("on change"); 
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-2">
         <textarea className="form-control" value={text} onChange={handleOnChange}  id="mybox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpclick}>convert_to_Uppercase</button>
-      <button className="btn btn-primary" onClick={handlelowclick}>convert_to_Lowercase</button>
+      <button className="btn btn-primary mx-1" onClick={handleUpclick}>convert_to_Uppercase</button>
+      <button className="btn btn-primary mx-1" onClick={handlelowclick}>convert_to_Lowercase</button>
     </div>
+    <div className="container my-3">
+      <h3>Text-Summary</h3>
+      <p>{text.split(" ").length} Words & {text.length} characters</p>
+      <h4>Preveiw</h4>
+      <p>{text}</p>
+    </div>
+
+    </>
   );
 }
  
